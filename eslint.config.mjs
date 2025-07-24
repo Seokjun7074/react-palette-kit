6bd3617cf45d6e485c2d9a4538de46ca4a5f5c2a
Seokjun7074 <sukjune96@gmail.com>
2025-07-25 01:58:44 +0900

    chore: eslint 설정 변경

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,32 +1,40 @@
-import js from '@eslint/js';
+import eslint from '@eslint/js';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
-import { defineConfig } from 'eslint/config';
 import unusedImports from 'eslint-plugin-unused-imports';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
-export default defineConfig([
-  { ignores: ['**/dist/', '**/esm/', '.pnp.*', '.yarn/'] },
+export default tseslint.config([
+  {
+    ignores: [
+      '**/dist/',
+      '**/esm/',
+      '.pnp.*',
+      '.yarn/',
+      '.vscode/',
+      '**/node_modules/',
+    ],
+  },
+  eslint.configs.recommended,
   tseslint.configs.recommended,
   {
     files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
-    plugins: { js, 'unused-imports': unusedImports },
-    extends: ['js/recommended'],
     languageOptions: { globals: globals.browser },
+  },
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'off',
+    },
+  },
+
+  {
+    plugins: {
+      'unused-imports': unusedImports,
+    },
     rules: {
       'no-unused-vars': 'off',
-      'no-undef': 'off',
       'unused-imports/no-unused-imports': 'warn',
-      'unused-imports/no-unused-vars': [
-        'warn',
-        {
-          vars: 'all',
-          varsIgnorePattern: '^_',
-          args: 'after-used',
-          argsIgnorePattern: '^_',
-        },
-      ],
-      '@typescript-eslint/no-unused-vars': 'off',
+      'unused-imports/no-unused-vars': 'warn',
     },
   },
   eslintPluginPrettierRecommended,
